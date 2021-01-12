e19b7158ff42de81800ffc33364f135267448037
ahyeon <ahyeonlog@gmail.com>
2021-01-13 04:06:20 +0900

    회원가입화면 구현

--- frontend/src/views/user/Login.jsx
+++ frontend/src/views/user/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import PropTypes from "prop-types";
 
 class Login extends Component {
@@ -6,25 +7,29 @@ class Login extends Component {
     const hasAccount = true;
     return (
       <section className="login">
-        <div className="loginContainer">
-          <h1>당신의 눈동자에 cheers...☆</h1>
-
-          <label>Username</label>
-          <input
-            type="text"
-            autoFocus
-            required
-            placeholder="이메일을 입력하세요"
-          />
-          <p className="errorMsg">이메일 형식을 확인하세요</p>
-
-          <label>Password</label>
-          <input type="password" required placeholder="비밀번호를 입력하세요" />
-
+        <div className="userContainer">
+          <p className="userAppTitle">
+            당신의
+            <br />
+            눈동자에 cheers...
+          </p>
+          <p className="userTitle">회원 서비스 이용을 위해 로그인 해주세요.</p>
+          <input type="text" autoFocus required placeholder="이메일" />
+          {/* <p className="errorMsg">이메일 형식을 확인하세요</p> */}
+          <input type="password" required placeholder="비밀번호" />
+          <div className="userLink">
+            <Link to="/user/resetpassword" className="userLink">
+              비밀번호 찾기
+            </Link>{" "}
+            |{" "}
+            <Link to="/user/join" className="userLink">
+              회원가입
+            </Link>
+          </div>
           <div className="btnContainer">
             {hasAccount ? (
               <div>
-                <button>Login</button>
+                <button>로그인하기</button>
               </div>
             ) : (
               <div>
@@ -32,7 +37,6 @@ class Login extends Component {
               </div>
             )}
           </div>
-          <p>혹시 비밀번호를 잊으셨나요?</p>
         </div>
       </section>
     );
--- frontend/src/views/user/ResetPassword.jsx
+++ frontend/src/views/user/ResetPassword.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import PropTypes from "prop-types";
 
-class SignUp extends Component {
+class ResetPassword extends Component {
   render() {
     return (
       <section className="login">
@@ -12,20 +12,21 @@ class SignUp extends Component {
             <br />
             눈동자에 cheers...
           </p>
-          <p className="userTitle">
-            회원 서비스 이용을 위해 회원가입을 해주세요.
-          </p>
+          <p className="userTitle">비밀번호 찾기</p>
+          <input type="text" autoFocus required placeholder="이름" />
           <input type="text" autoFocus required placeholder="이메일" />
-          {/* <p className="errorMsg">이메일 형식을 확인하세요</p> */}
-          <input type="password" required placeholder="비밀번호" />
-          <input type="password" required placeholder="비밀번호확인" />
           <div className="userLink">
             <Link to="/" className="userLink">
-              이미 계정이 있으신가요?
+              로그인
+            </Link>{" "}
+            |{" "}
+            <Link to="/user/join" className="userLink">
+              회원가입
             </Link>
           </div>
+
           <div className="btnContainer">
-            <button>회원가입하기</button>
+            <button>비밀번호 찾기</button>
           </div>
         </div>
       </section>
@@ -33,4 +34,4 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp;
+export default ResetPassword;
