7415f86092eb160da01fc709bef0a49931d27a24
ahyeon <ahyeonlog@gmail.com>
2021-01-14 20:31:18 +0900

    server

---
+++ backend/src/main/java/com/web/curation/model/user/ChangePasswordRequest.java
@@ -0,0 +1,17 @@
+package com.web.curation.model.user;
+
+
+import lombok.*;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+    
+    String email;
+    String password;
+    String newPassword;
+    
+}
---
+++ backend/src/main/java/com/web/curation/model/user/User.java
@@ -0,0 +1,39 @@
+// 하단 DB 설정 부분은 Sub PJT II에서 데이터베이스를 구성한 이후에 주석을 해제하여 사용.
+
+package com.web.curation.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//Json 형식으로 데이터를 주고받을 때 jackson의 ObjectMapper를 자주 이용한다.
+//JsonInclude는 Json 형식의 데이터를 어떻게 만들지를 정하는 Annotation이다.
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nickname;
+
+    @JsonIgnore
+    private String password;
+    private String email;
+
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime createDate;
+
+}
