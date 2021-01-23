5d3dc7ec8b070f6945ff81a87a96738be0a91512
tnalsvjscl <tnalsvjscl@naver.com>
2021-01-23 13:37:16 +0900

    fix : 스웨거 pahtvariable 오류수정
    feat : 스웨거 업데이트 / feed list

--- backend/src/main/java/com/web/curation/controller/account/AccountController.java
+++ backend/src/main/java/com/web/curation/controller/account/AccountController.java
@@ -1,8 +1,6 @@
 package com.web.curation.controller.account;
 
 import java.util.List;
-import java.util.Map;
-import java.time.LocalDateTime;
 import java.util.Optional;
 import javax.validation.Valid;
 
@@ -19,13 +17,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,9 +27,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 //@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
@@ -53,9 +46,8 @@ public class AccountController {
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
-	@PostMapping("")
+	@PostMapping
 	@ApiOperation(value = "회원가입")
-
 	public Object signup(
 			@Valid @RequestBody @ApiParam(value = "회원가입 시 필요한 회원정보(이메일, 별명, 비밀번호).", required = true) SignupRequest request) {
 		// 이메일, 닉네임 중복처리 필수
@@ -64,10 +56,10 @@ public class AccountController {
 		String nickname = request.getNickname().trim();
 		String password = request.getPassword().trim();
 
-		User curUser = userDao.getUserByEmail(email);
+		// User curUser = userDao.getUserByEmail(email);
 		// 이메일 중복 체크
 		if (userDao.getUserByEmail(email) != null) {
-			
+
 			return makeResponse("400", null, "this email already exists", HttpStatus.BAD_REQUEST);
 		}
 		// 이메일, 별명, 패스워드 비어있는지 확인
@@ -78,11 +70,7 @@ public class AccountController {
 		if (userDao.getUserByNickname(nickname) != null)
 			return makeResponse("400", null, "this nickname already exists", HttpStatus.BAD_REQUEST);
 
-		User user = User.builder()
-				.email(email)
-				.password(password)
-				.nickname(nickname)
-				.build();
+		User user = User.builder().email(email).password(password).nickname(nickname).build();
 
 		User savedUser = userDao.save(user);
 
@@ -91,7 +79,6 @@ public class AccountController {
 
 	@PostMapping("/login")
 	@ApiOperation(value = "로그인", notes = "아이디와 비밀번호를 받아 로그인을 합니다.")
-
 	public Object login(
 			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) AuthenticationRequest request) {
 		String email = request.getEmail().trim();
@@ -112,7 +99,6 @@ public class AccountController {
 
 	@PutMapping("/password")
 	@ApiOperation(value = "비밀번호 변경")
-
 	public Object changePassword(
 			@Valid @RequestBody @ApiParam(value = "비밀번호 변경 시 필요한 회원정보(이메일, 기존 비밀번호, 새 비밀번호).", required = true) ChangePasswordRequest request) {
 		User curUser = userDao.getUserByEmail(request.getEmail());
@@ -133,7 +119,7 @@ public class AccountController {
 		}
 	}
 
-	@PutMapping("")
+	@PutMapping
 	@ApiOperation(value = "회원 수정")
 	public Object update(
 			@Valid @RequestBody @ApiParam(value = "회원 정보 수정(닉네임, 한줄 소개).", required = true) UpdateRequest request) {
@@ -162,9 +148,9 @@ public class AccountController {
 		return makeResponse("200", convertObjToJson(curUser), "success", HttpStatus.OK);
 	}
 
-	@GetMapping("/{nickname}")
-	@ApiOperation(value = "닉네임으로 검색", notes = "닉네임에 검색 키워드가 포함이 된 모든 유저 정보를 리스트로 반환합니다. \n "
-			+ "검색 키워드에 해당되는 정보가 없다면 404 에러와 data에 null 값이 담깁니다.")
+	@GetMapping("/search/{nickname}")
+	@ApiOperation(value = "닉네임으로 검색", notes = "닉네임에 검색 키워드가 포함이 된 모든 유저 정보를 리스트로 반환합니다. "
+			+ "검색 키워드에 해당되는 정보가 없다면 404 에러와 data에 null 값이 담깁니다s.")
 	public Object searchByNickname(
 			@Valid @ApiParam(value = "닉네임으로 검색", required = true) @PathVariable String nickname) {
 		List<User> searchResult = userDao.findByNicknameContaining(nickname);
@@ -175,7 +161,7 @@ public class AccountController {
 		return makeResponse("200", convertObjToJson(searchResult), "success", HttpStatus.OK);
 	}
 
-	@DeleteMapping("")
+	@DeleteMapping
 	@ApiOperation(value = "회원 삭제")
 	public Object delete(
 			@Valid @RequestBody @ApiParam(value = "회원정보 탈퇴 시 필요한 회원정보(이메일, 별명, 비밀번호).", required = true) SignupRequest request) {
