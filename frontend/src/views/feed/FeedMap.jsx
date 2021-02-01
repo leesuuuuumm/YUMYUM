bc3b3d6eef7f29b9d1cf45f76537bc8c5bfd0c7f
염성훈 <lovelove33j1@naver.com>
2021-02-01 22:28:26 +0900

    refactor: getListItem 함수 분리

--- frontend/src/views/feed/FeedMap.jsx
+++ frontend/src/views/feed/FeedMap.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./CSS/FeedMap.css";
-import { Link, withRouter } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { withRouter } from "react-router-dom";
 import ArrowForwardRoundedIcon from "@material-ui/icons/ArrowForwardRounded";
+import { getListItem } from "../../_components/map/getListItem";
 
 const { kakao } = window;
 
@@ -132,37 +132,6 @@ const FeedMap = (props) => {
     menuEl.scrollTop = 0;
     // 검색된 장소 위치를 기준으로 지도 범위 재설정
     map.setBounds(bounds);
-  }
-
-  function getListItem(index, places) { //TODO : places로 바꿔줘야한다. 
-    var el = document.createElement("li"),
-      itemStr =
-        '<span class="markerbg marker_' +
-        (index + 1) +
-        '"></span>' +
-        '<div class="info">' +
-        "   <h5>" +
-        places.place_name +
-        "</h5>";
-
-    if (places.road_address_name) {
-      itemStr +=
-        "    <span>" +
-        places.road_address_name +
-        "</span>" +
-        '   <span class="jibun gray">' +
-        places.address_name +
-        "</span>";
-    } else {
-      itemStr += "    <span>" + places.address_name + "</span>";
-    }
-
-    itemStr += '  <span class="tel">' + places.phone + "</span>" + "</div>";
-
-    el.innerHTML = itemStr;
-    el.className = "item";
-
-    return el;
   }
   // map에 마커를 찍는 함수
   function addMarker(position, idx, title) {
