f415fd7e387e1d0c0c3afe8a6394ef803e18688a
정준현 <jwnsugs@gmail.com>
2021-02-10 13:57:48 +0900

    feat : jwtService 수정

--- backend/src/main/java/com/web/curation/service/jwt/JwtServiceImpl.java
+++ backend/src/main/java/com/web/curation/service/jwt/JwtServiceImpl.java
@@ -1,27 +1,30 @@
 package com.web.curation.service.jwt;
 
 import com.web.curation.exception.UnauthorizedException;
-
-import io.jsonwebtoken.*;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
-
-import java.util.*;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtServiceImpl implements JwtService {
-
     public static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
+    @Value("&{tokenSecret}")
+    String TK;
 
-    private static final String TK = "ssafySecret";
     private static final int EXPIRE_MINUTES = 60;
+    public String token;
 
     @Override
     public <T> String create(String key, T data, String subject) {
@@ -46,20 +49,20 @@ public class JwtServiceImpl implements JwtService {
         return key;
     }
 
-    //	전달 받은 토큰이 제대로 생성된것인지 확인 하고 문제가 있다면 UnauthorizedException을 발생.
+    //   전달 받은 토큰이 제대로 생성된것인지 확인 하고 문제가 있다면 UnauthorizedException을 발생.
     @Override
     public boolean isUsable(String jwt) {
         try {
             Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
             return true;
         } catch (Exception e) {
-//			if (logger.isInfoEnabled()) {
-//				e.printStackTrace();
-//			} else {
+//         if (logger.isInfoEnabled()) {
+//            e.printStackTrace();
+//         } else {
             logger.error(e.getMessage());
-//			}
-//			throw new UnauthorizedException();
-//			개발환경
+//         }
+//         throw new UnauthorizedException();
+//         개발환경
             return false;
         }
     }
@@ -77,24 +80,25 @@ public class JwtServiceImpl implements JwtService {
         try {
             claims = Jwts.parser().setSigningKey(TK.getBytes("UTF-8")).parseClaimsJws(jwt);
         } catch (Exception e) {
-//			if (logger.isInfoEnabled()) {
-//				e.printStackTrace();
-//			} else {
+//         if (logger.isInfoEnabled()) {
+//            e.printStackTrace();
+//         } else {
             logger.error(e.getMessage());
-//			}
+//         }
             throw new UnauthorizedException();
-//			개발환경
-//			Map<String,Object> testMap = new HashMap<>();
-//			testMap.put("userid", userid);
-//			return testMap;
+//         개발환경
+//         Map<String,Object> testMap = new HashMap<>();
+//         testMap.put("userid", userid);
+//         return testMap;
         }
         Map<String, Object> value = claims.getBody();
         logger.info("value : {}", value);
         return value;
     }
 
+
     @Override
     public String getUserEmail() {
-        return (String) this.get("user").get("email");
+        return (String)this.get("email").get("email");
     }
 }
