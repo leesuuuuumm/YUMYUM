3ad820d1664799cc8543e68e3447bc362972263c
염성훈 <lovelove33j1@naver.com>
2021-02-17 16:57:10 +0900

    fix : 머지해결

--- frontend/src/views/feed/FeedMap.jsx
+++ frontend/src/views/feed/FeedMap.jsx
@@ -281,17 +281,6 @@ const FeedMap = (props) => {
         },
       })
     }
-
-  }
-
-  // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-  function zoomIn() {
-    map.setLevel(map.getLevel() - 1);
-  }
-
-  // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-  function zoomOut() {
-    map.setLevel(map.getLevel() + 1);
   }
 
   return (
